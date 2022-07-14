8f993e305f1cf1b4e64f882d1b9cd323230bdbb0
keshkaush <keshav.kaushik344@gmail.com>
2022-07-14 21:39:07 +0530

    BFS creating graph

--- breadth_first_search.py
+++ breadth_first_search.py
@@ -1,3 +1,17 @@
 # breadth first search answer 2 type of questions:
 # 1. is there a path from node A to node B.
 # 2. if there a path then find the shortest path from node A to B.
+# it does not take weighted graph that is for diskastra algorithm
+# basically it gives a path with that contains the less number of edges or nodes.
+# directed graph put restriction on nodes meanwhile in undirected graph data can flow in any direction.
+
+graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["thom"] = []
+graph["jonny"] = []
+graph["anuj"] = []
+graph["peggy"] = []
+
