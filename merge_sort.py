b0486fcb41f43a872461af2921f178b1d7e7f01e
keshkaush <keshav.kaushik344@gmail.com>
2022-07-17 23:30:04 +0530

    Adding merge sort algorithm.

---
+++ merge_sort.py
@@ -0,0 +1,14 @@
+# algo
+# step 1: start
+# step 2: declare array and left, right, mid variable 
+# step 3: perform merge function.
+#         mergesort(array,left,right)
+#         mergesort (array, left, right)
+#         if left > right
+#         return
+#         mid= (left+right)/2
+#         mergesort(array, left, mid)
+#         mergesort(array, mid+1, right)
+#         merge(array, left, mid, right)
+# step 4: Stop
+
